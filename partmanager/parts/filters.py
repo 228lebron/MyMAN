6d91e77cb1fa3fa873f37cfb94bd8d455f58f0df
fuck <justfly50@yandex.ru>
2023-02-10 16:01:12 +0300

    -req and quotas

--- partmanager/parts/filters.py
+++ partmanager/parts/filters.py
@@ -2,10 +2,10 @@ import django_filters
 from .models import *
 
 class ReqFilter(django_filters.FilterSet):
-    part_number__number = django_filters.CharFilter(lookup_expr='icontains', label='Part number')
-    brand = django_filters.CharFilter(lookup_expr='icontains', label='Brand')
-    date = django_filters.DateFilter(label='Date')
+    request__part_number__number = django_filters.CharFilter(lookup_expr='icontains', label='Part number')
+    request__brand = django_filters.CharFilter(lookup_expr='icontains', label='Brand')
+    request__date = django_filters.DateFilter(label='Date')
 
     class Meta:
         model = RequestQuotaResult
-        fields = ['request', 'quota', 'part_number__number', 'brand', 'date']
+        fields = ['request', 'quota', 'request__part_number__number', 'request__brand', 'request__date']
